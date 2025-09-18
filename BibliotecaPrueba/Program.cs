da2839f3667349d44eeded5202ca27013d46fcb0
nicoalazar <nicozalazar67@gmail.com>
2025-09-17 22:41:41 -0300

    Listo para entregar

--- BibliotecaPrueba/Program.cs
+++ BibliotecaPrueba/Program.cs
@@ -1,27 +1,48 @@
-﻿using BibliotecaPrueba;
-
-public class Program
+﻿namespace BibliotecaPrueba
 {
-    public static void Main()
+    public class Program
     {
-        Biblioteca b = new Biblioteca();
-        b.AgregarLibro("Cien años de soledad", "García Márquez", "Sudamericana");
-        b.AgregarLibro("El Principito", "Saint-Exupéry", "Reynal & Hitchcock");
-        b.AgregarLibro("Ficciones", "Borges", "Sur");
-        b.AgregarLibro("Rayuela", "Cortázar", "Sudamericana");
-        b.AgregarLibro("Pedro Páramo", "Rulfo", "FCE");
+        public static void Main()
+        {
+            Biblioteca b = new Biblioteca();
+            Console.WriteLine("--------Inicio de Programa-------");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Se comprueban libros y lectores:");
+            b.ListarLibros();
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Se agregan 5 libros y se listan.");
+            b.AgregarLibro("Cien años de soledad", "García Márquez", "Sudamericana");
+            b.AgregarLibro("El Principito", "Saint-Exupéry", "Reynal & Hitchcock");
+            b.AgregarLibro("Ficciones", "Borges", "Sur");
+            b.AgregarLibro("Rayuela", "Cortázar", "Sudamericana");
+            b.AgregarLibro("Pedro Páramo", "Rulfo", "FCE");
+            b.ListarLibros();
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Se agregan dos lectores, 'Ana Perez' y 'Juan Gomez'");
+            b.AltaLector("Ana Perez", "30111222");
+            b.AltaLector("Juan Gomez", "28123456");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Ana pide un libro");
+            Console.WriteLine(b.PrestarLibro("El Principito", "30111222")); // PRESTAMO EXITOSO
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Juan pide el mismo libro que Ana");
+            Console.WriteLine(b.PrestarLibro("El Principito", "28123456")); // LIBRO INEXISTENTE
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Una persona no registrada pide un libro");
+            Console.WriteLine(b.PrestarLibro("Ficciones", "99999999"));     // LECTOR INEXISTENTE
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Ana pide 2 libros mas");
+            Console.WriteLine(b.PrestarLibro("Cien años de soledad", "30111222")); // OK // PRESTAMO EXITOSO
+            Console.WriteLine(b.PrestarLibro("Rayuela", "30111222")); // OK (3er préstamo) // // PRESTAMO EXITOSO
+            Console.WriteLine("----------------------------------");
+            b.ListarLibros();
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Ana intenta pedir un cuarto libro");
+            Console.WriteLine(b.PrestarLibro("Pedro Páramo", "30111222")); // TOPE DE PRESTAMO ALCAZADO
+            Console.WriteLine("--------------------------------");
+            b.ListarLibros();
+            Console.WriteLine("---------Fin de Programa--------");
 
-        b.AltaLector("Ana Pérez", "30111222");
-        b.AltaLector("Juan Gómez", "28123456");
-
-        Console.WriteLine(b.PrestarLibro("El Principito", "30111222")); // PRESTAMO EXITOSO
-        Console.WriteLine(b.PrestarLibro("El Principito", "28123456")); // LIBRO INEXISTENTE
-        Console.WriteLine(b.PrestarLibro("Ficciones", "99999999"));     // LECTOR INEXISTENTE
-
-        // Llevar a Ana al tope
-        Console.WriteLine(b.PrestarLibro("Cien años de soledad", "30111222")); // OK // PRESTAMO EXITOSO
-        Console.WriteLine(b.PrestarLibro("Rayuela", "30111222")); // OK (3er préstamo) // // PRESTAMO EXITOSO
-        Console.WriteLine(b.PrestarLibro("Pedro Páramo", "30111222")); // TOPE DE PRESTAMO ALCAZADO
-    
-}
+        }
+    }
 }
