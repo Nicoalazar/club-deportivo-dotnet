1dccd2f6644ee3f122a9e31d64041b208a7b47c4
nicoalazar <nicozalazar67@gmail.com>
2025-10-06 20:30:59 -0300

    coment btnIngresar_Click

--- ClubDeportivo/FrmLogin .cs
+++ ClubDeportivo/FrmLogin .cs
@@ -14,16 +14,33 @@ namespace ClubDeportivo
         {
             InitializeComponent();
         }
+        //TODO: DESCOMENTAR EN DEPLOY
 
+        //private void btnIngresar_Click(object sender, EventArgs e)
+        //{
+        //    DataTable tablaLogin = new DataTable(); // es la que recibe los  datos desde el formulario
+        //    Usuario dato = new Usuario(); // variable que contiene todas las caracteristicas de la clase
+        //    tablaLogin = dato.Log_Usu(txtUsuario.Text, txtClave.Text);
+        //    if (tablaLogin.Rows.Count > 0)
+        //    {
+        //        // quiere decir que el resultado tiene 1 fila por lo que el usuario EXISTE
+        //        MessageBox.Show("Ingreso exitoso");
+        //        var frm = new FrmPrimerProyecto();
+        //        frm.Show();
+        //        this.Hide();
+        //        frm.FormClosed += (s, args) => this.Close();
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("Usuario y/o password incorrecto");
+        //    }
+        //}
+
+        //TODO: ELIMINAR EN DEPLOY
         private void btnIngresar_Click(object sender, EventArgs e)
         {
-            DataTable tablaLogin = new DataTable(); // es la que recibe los  datos desde el formulario
-            Usuario dato = new Usuario(); // variable que contiene todas las caracteristicas de la clase
-            tablaLogin = dato.Log_Usu(txtUsuario.Text, txtClave.Text);
-            if (tablaLogin.Rows.Count > 0)
+            if (txtUsuario.Text == "ADMIN" && txtClave.Text == "1234")
             {
-                // quiere decir que el resultado tiene 1 fila por lo que el usuario EXISTE
-                MessageBox.Show("Ingreso exitoso");
                 var frm = new FrmPrimerProyecto();
                 frm.Show();
                 this.Hide();
@@ -31,7 +48,8 @@ namespace ClubDeportivo
             }
             else
             {
-                MessageBox.Show("Usuario y/o password incorrecto");
+                MessageBox.Show("Usuario inexistente.", "Acceso denegado",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
     }
