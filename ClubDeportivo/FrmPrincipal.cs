b22f466f6c0aa11792b14174b523d42b1a467d2d
unknown <albor77@gmail.com>
2025-10-08 02:38:13 -0300

    agrego la ventana principal con los distintos botones

---
+++ ClubDeportivo/FrmPrincipal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClubDeportivo
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrarPersona_Click(object sender, EventArgs e)
+        {
+            FrmPrimerProyecto frmAltas = new FrmPrimerProyecto();
+            frmAltas.ShowDialog();
+        }
+
+        // dejo los botones q todavia no pidieron sin funcionalidad. 
+        private void btnListarVencimientos_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Funcionalidad 'Listar Vencimientos' - ¡PRÓXIMAMENTE!",
+                    "En Desarrollo",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+        }
+
+        private void btnRevisarAsistencia_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Funcionalidad 'Revisar Asistencia' - ¡PRÓXIMAMENTE!",
+                "En Desarrollo",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+        private void btnPrueba_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Funcionalidad 'Prueba' - ¡PRÓXIMAMENTE!",
+                "En Desarrollo",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+    }
+}
