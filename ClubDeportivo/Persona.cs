25f29f16e26b0a181ecc12db800a63ac12331501
LauBelen <green.lau@hotmail.com>
2025-10-08 20:20:50 -0300

    Declare el método MostrarDatos en Persona para herencia de Socio y NoSocio

--- ClubDeportivo/Persona.cs
+++ ClubDeportivo/Persona.cs
@@ -5,7 +5,7 @@
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string Tipo { get; set; }       // DNI / Pasaporte / Extranjero
-        public string Documento { get; set; }        
+        public string Documento { get; set; }
         public string Email { get; set; }
         public int Telefono { get; set; }
 
@@ -23,5 +23,11 @@
             Email = email;
             Telefono = telefono;
         }
+        
+        public virtual string MostrarDatos()
+{
+    return $"{Nombre} {Apellido} | {Tipo}: {Documento} | Tel: {Telefono} | Email: {Email}";
+}
+
     }
 }
