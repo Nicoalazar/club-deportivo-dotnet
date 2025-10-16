6c6d2ce9d8af336fe3743448c4993ecfdea168aa
nicoalazar <nicozalazar67@gmail.com>
2025-10-16 18:00:10 -0300

    S10_Polimorfismo

---
+++ S10_Polimorfismo/Regulares.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10_Polimorfismo
+{
+    internal class Regulares: Paquete
+    {
+        public Regulares(double pesoKg, double dimensionesM2, string destino)
+            : base(pesoKg, dimensionesM2, destino)
+        {
+        }
+        public override double CalcularCostoDeEnvio()
+        {
+            return pesoKg * 10; // $10 por kg
+        }
+        public override void MostrarInformacionDetallada()
+        {
+            Console.WriteLine("Paquete Regular");
+            base.MostrarInformacionDetallada();
+            Console.WriteLine($"Costo de envío: ${CalcularCostoDeEnvio():F2}\n");
+        }
+    }
+}
