385e8c7e2a165edef01f3a4f65486e07b113db91
nicoalazar <nicozalazar67@gmail.com>
2025-11-02 23:12:52 -0300

    S13

---
+++ S13_Atributos_Y_Metodos/Circulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S13_Atributos_Y_Metodos
+{
+    public class Circulo
+    {
+        public static double Radio { get; set; }
+
+        public static double CalcularArea()
+        {
+            return Math.PI * Math.Pow(Radio, 2);
+        }
+
+        public static double CalcularPerimetro()
+        {
+            return 2 * Math.PI * Radio;
+        }
+    }
+}
